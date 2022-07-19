13a0d5459cfb8f32deb9cf97fcce5d2ee5e516f4
AdrissonV <adrissonvinicius02@gmail.com>
2022-07-18 23:31:48 -0300

    Dados da API consumidos e manipulados

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Realizar a conexão HTTP buscando o TOP 250 filmes
+        String url = "https://api.mocki.io/v2/549a5d8b";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // Extrair ou parsear os dados que precisamos (titulo, poster e rating)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaFilmes = parser.parse(body);
+
+        // Exibir e manipular os dados
+        for (Map<String, String> filme : listaFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+
     }
 }
